927e112c41144e96608fa4fdbcc0907b891c378b
Vinicius Alves Fonseca <vinicius.fonseca@veripag.com.br>
2020-02-07 17:23:56 -0300

    fix publish configs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { createClass } from './createClass'
 export { createStyle } from './createStyle'
 export { genCode } from './genCode'
-export { useCSSClass } from './useCSSClass'
+export { useCSSClass, css } from './useCSSClass'
